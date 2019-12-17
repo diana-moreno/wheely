234286e4956adba90cbb1cefbdb0b3d4eb7acb43
Diana <d7@hotmail.es>
2019-12-17 20:10:33 +0100

    Updates

---
+++ precache-manifest.9dea384eb64edde842fb650154a8c6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6acfb75703b13769627c6739e1deff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00971a4d7b81df70baa8",
+    "url": "/static/css/main.27174fa5.chunk.css"
+  },
+  {
+    "revision": "e05996e59512f523abe6",
+    "url": "/static/js/2.4463e062.chunk.js"
+  },
+  {
+    "revision": "00971a4d7b81df70baa8",
+    "url": "/static/js/main.0b9fa8bb.chunk.js"
+  },
+  {
+    "revision": "3aaa71871504f36aeb48",
+    "url": "/static/js/runtime-main.0e289f90.js"
+  },
+  {
+    "revision": "7661e0a3d6832d8cd10a4a7d740f8fbe",
+    "url": "/static/media/asphalt-wheel.7661e0a3.jpg"
+  }
+]);
